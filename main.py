9ebf04a298b244b75932cfc56e893a7b7e40e07c
Batyrkhan07 <77788604+Batyrkhan07@users.noreply.github.com>
2021-01-21 19:57:10 +0600

    First Commit

---
+++ main.py
@@ -0,0 +1,52 @@
+# z = datetime.date(18, 1, 2021)
+# delta = datetime.timedelta = 3
+t = 395
+tovar = 390
+tovar -= 100
+tovar2 = 100
+tovar3 = tovar + tovar2
+print(t % tovar)
+
+
+
+print ('hi')
+f = float(input('введите первое число>>'))
+oper = input("введите операцию>>")
+s = float(input("введите второе число>>"))
+oper = input("введите операцию>>")
+th = float(input("введите четвертое число>>"))
+oper = input("введите операцию>>")
+p = float(input('ввидите пятое чило>>'))
+if oper == '+' :
+        print(f + s)
+elif oper == '-':
+        print(f - s)
+elif oper == '*':
+        print(f * s)
+elif oper == '/':
+        print(f / s)
+else:
+        print("Error")
+
+
+
+
+x = 10
+y = 150
+x,y = y,x
+print('x =', y)
+print('y =', x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
